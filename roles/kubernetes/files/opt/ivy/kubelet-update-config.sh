398aaa9aa4a60da81365dae2919b9c51efd6cfe7
Ricardo Rosales <ricardo.rosales@over-haul.com>
2022-07-24 11:41:34 -0500

    [P20-1696] [kubernetes] Removing deprecated kube-apiserver flag insecure-port Adding script /opt/ivy/kubelet-update-config.sh that will update /etc/kubernetes/kubelet/config.yaml with cluster-domain, cluster-dns and provider-id since these flags have been deprecated from kubelet Removing deprecated kubelet flag network-plugin

---
+++ roles/kubernetes/files/opt/ivy/kubelet-update-config.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+# shellcheck disable=SC1091
+. /opt/ivy/bash_functions.sh
+
+THIS_SCRIPT=$(basename "${0}")
+PADDING=$(printf %-${#THIS_SCRIPT}s " ")
+
+function usage () {
+  echo "Usage:"
+  echo "${THIS_SCRIPT} -c,--config <kubelet config file to update, default: /etc/kubernetes/kubelet/config.yaml>"
+  echo "${PADDING} --cluster-domain <clusterDomain is the DNS domain for this cluster>"
+  echo "${PADDING} --cluster-dns <clusterDNS IP addresses for the cluster DNS server, repeat this parameter per IP>"
+  echo "${PADDING} --provider-id <providerID, if set, sets the unique ID of the instance that an external provider (i.e. cloudprovider) can use to identify a specific node>"
+  echo
+  echo "Update kubelet config file"
+  exit
+}
+
+declare -a CLUSTER_DNS=()
+KUBELET_KUBECONFIG='/etc/kubernetes/kubelet/config.yaml'
+while [[ $# -gt 0 ]]; do
+  case "${1}" in
+      -c|--config)
+        KUBELET_KUBECONFIG="${2}"
+        shift # past argument
+        shift # past value
+        ;;
+      --cluster-domain)
+        CLUSTER_DOMAIN="${2}"
+        shift # past argument
+        shift # past value
+        ;;
+      --cluster-dns)
+        CLUSTER_DNS+=("${2}")
+        shift # past argument
+        shift # past value
+        ;;
+      --provider-id)
+        PROVIDER_ID="${2}"
+        shift # past argument
+        shift # past value
+        ;;
+      -*)
+        echo "Unknown option ${1}"
+        usage
+        ;;
+  esac
+done
+
+if [[ -z ${CLUSTER_DOMAIN:-""} ]] || [[ -z ${CLUSTER_DNS:-""} ]] || [[ -z ${PROVIDER_ID:-""} ]]; then
+  usage
+fi
+
+yq e -i ".clusterDomain = \"${CLUSTER_DOMAIN}\"" "${KUBELET_KUBECONFIG}"
+
+for dns in "${CLUSTER_DNS[@]}"; do
+  yq e -i ".clusterDNS += \"${dns}\"" "${KUBELET_KUBECONFIG}"
+done
+
+yq e -i ".providerID = \"${PROVIDER_ID}\"" "${KUBELET_KUBECONFIG}"
